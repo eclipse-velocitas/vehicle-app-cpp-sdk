526c1583bf37d320e9795ef66a06dd810e04baaf
BjoernAtBosch <Bjoern.Hornburg@de.bosch.com>
2023-05-04 08:48:25 +0200

    Restore feeder configuration

--- .vscode/scripts/runtime/local/run-feedercan.sh
+++ .vscode/scripts/runtime/local/run-feedercan.sh
@@ -24,7 +24,7 @@ UTILS_DIRECTORY="$ROOT_DIRECTORY/.vscode/scripts/runtime/utils"
 source $UTILS_DIRECTORY/get-appmanifest-data.sh
 
 ### Override default files for feedercan
-CONFIG_DIR="$ROOT_DIRECTORY/deploy/runtime/k3d/volume"
+CONFIG_DIR="$ROOT_DIRECTORY/.vscode/scripts/runtime/feeder_config/default"
 
 export VEHICLEDATABROKER_DAPR_APP_ID=vehicledatabroker
 export LOG_LEVEL=info,databroker=info,dbcfeeder.broker_client=info,dbcfeeder=info
