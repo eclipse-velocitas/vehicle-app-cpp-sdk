2ec71cde8498259007e9954ac50666bf6c49aa96
BjoernAtBosch <Bjoern.Hornburg@de.bosch.com>
2023-05-23 15:57:52 +0200

    Put everything in requires list

--- conanfile.py
+++ conanfile.py
@@ -23,6 +23,9 @@ class VehicleAppCppSdkConan(ConanFile):
     license = "Apache-2.0"
     url = "https://github.com/eclipse-velocitas/vehicle-app-cpp-sdk"
     description = "The Vehicle App SDK for c++ allows to create Vehicle Apps from the Velocitas development model in the c++ programming language."
+    # Workaround1: Pin recipe revision for transient dependency googleapis for enabling the container build
+    # Workaround2: Pin recipe revision for transient dependency paho-mqtt-c cause latest is pulling libanl which cannot be found
+    requires = "openssl/1.1.1t", "nlohmann_json/3.11.2", "paho-mqtt-cpp/1.2.0", "grpc/1.48.0", "protobuf/3.21.9", "cpr/1.9.3", "fmt/9.1.0", "googleapis/cci.20221108@#e4bebdfa02f3b6f93bae1d5001b8d439", "paho-mqtt-c/1.3.9@#0421671a9f4e8ccfa5fc678cfb160394"
     generators = "cmake"
     author = "Robert Bosch GmbH"
 
@@ -78,27 +81,8 @@ class VehicleAppCppSdkConan(ConanFile):
         self.copy("license*", src=".", dst="./licenses",
                   folder=True, ignore_case=True)
 
-    def requirements(self):
-        # Direct dependencies:
-        self.requires("openssl/1.1.1q")
-        self.requires("libcurl/7.84.0")
-        self.requires("nlohmann_json/3.11.2")
-        self.requires("paho-mqtt-cpp/1.2.0")
-        self.requires("grpc/1.48.0")
-        self.requires("protobuf/3.21.4")
-        self.requires("cpr/1.9.0")
-        self.requires("fmt/9.1.0")
-        self.requires("zlib/1.2.13")
-
-        # Transient dependencies:
-        # self.requires("openssl/1.1.1t")
-        # Workaround: Pin recipe revision for transient dependency googleapis for enabling the container build
-        self.requires("googleapis/cci.20221108@#e4bebdfa02f3b6f93bae1d5001b8d439")
-        # Workaround: 
-        self.requires("paho-mqtt-c/1.3.9@#0421671a9f4e8ccfa5fc678cfb160394")
-
     def build_requirements(self):
         # 'build' context (protoc.exe will be available)
-        self.tool_requires("protobuf/3.21.4")
+        self.tool_requires("protobuf/3.21.9")
         self.tool_requires("grpc/1.48.0")
 
