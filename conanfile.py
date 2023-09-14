8ef265da44f84503cf8601de2716cb5196bd0a95
Dominic Sudy <99014187+doosuu@users.noreply.github.com>
2023-09-14 14:36:52 +0200

    fix: Dynamic versioning should cut off initial v on prefixed semver (#70)

--- conanfile.py
+++ conanfile.py
@@ -16,6 +16,7 @@ from conans import ConanFile, tools
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 import subprocess
 import os
+import re
 
 class VehicleAppCppSdkConan(ConanFile):
     name = "vehicle-app-sdk"
@@ -46,16 +47,22 @@ class VehicleAppCppSdkConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    
+
     exports = "version.txt"
 
     # Sources are located in the same place as this recipe, copy them to the recipe
     exports_sources = ".scripts/common.sh", "build.sh", "install_dependencies.sh", "CMakeLists.txt", "sdk/*", "examples/*", "conanfile.py", ".conan/profiles/*", "version.txt"
-    
+
     def set_version(self):
         try:
             git = tools.Git(folder=".")
-            version = git.get_tag() if git.get_tag() is not None else git.get_branch()
+            tag = git.get_tag()
+            if tag is not None:
+                version_tag_pattern = re.compile(r"^v[0-9]+(\.[0-9]+){0,2}")
+                if version_tag_pattern.match(tag):
+                    tag = tag[1:] # cut off initial v if a semver tag
+
+            version = tag if tag is not None else git.get_branch()
             if version == "HEAD (no branch)":
                 version = git.get_commit()
             open("./version.txt", mode="w", encoding="utf-8").write(version)
