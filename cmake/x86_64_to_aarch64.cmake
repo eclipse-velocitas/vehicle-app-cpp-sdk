91c02426b0d417906cce79124ca5590675698330
Markus Petke <MP91@users.noreply.github.com>
2024-03-06 09:20:14 +0100

    fix profile

--- cmake/x86_64_to_aarch64.cmake
+++ cmake/x86_64_to_aarch64.cmake
@@ -2,14 +2,14 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(TARGET_HOST aarch64-linux-gnu)
+set(TARGET_HOST aarch64-alpine-linux-musl-gcc)
 
 # which compilers to use for C and C++
-set(CMAKE_C_COMPILER   ${TARGET_HOST}-gcc)
-set(CMAKE_CXX_COMPILER ${TARGET_HOST}-g++)
+set(CMAKE_C_COMPILER   ${TARGET_HOST}-alpine-linux-musl-gcc)
+set(CMAKE_CXX_COMPILER ${TARGET_HOST}-alpine-linux-musl-gcc)
 
 # where is the target environment located
-set(CMAKE_FIND_ROOT_PATH  /usr/bin/aarch64-linux-gnu)
+set(CMAKE_FIND_ROOT_PATH  /usr/bin/aarch64-alpine-linux-musl-gcc)
 
 # adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment
