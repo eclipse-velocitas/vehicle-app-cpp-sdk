955f6bd6750a3a2861adb92180cbde131a1b4b5a
Dominic Sudy <dominic.sudy@bosch.com>
2024-03-18 17:49:59 +0100

    bugfix: Remove conflicting protobuf dependency

--- build.sh
+++ build.sh
@@ -30,6 +30,7 @@ Arguments:
 -t <name>, --target <name>       Builds only the target <name> instead of all targets. 
 -no-examples                     Disables the build of the SDK examples.
 -no-tests                        Disables the build of the SDK tests.
+--cov                            Generates coverage information.
 -s, --static                     Links all dependencies statically. 
 -x, --cross <arch>               Cross compiles for the specified architecture.
 -h, --help                       Shows this help.
@@ -43,6 +44,7 @@ BUILD_TARGET=all
 STATIC_BUILD=OFF
 SDK_BUILD_EXAMPLES=ON
 SDK_BUILD_TESTS=ON
+GEN_COVERAGE=OFF
 
 POSITIONAL_ARGS=()
 
@@ -78,6 +80,10 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
+    --cov)
+      GEN_COVERAGE=ON
+      shift
+      ;;
     -h|--help)
       print_help
       exit 0
@@ -102,11 +108,16 @@ echo "Host arch          ${HOST_ARCH}"
 echo "Build target       ${BUILD_TARGET}"
 echo "Build SDK examples ${SDK_BUILD_EXAMPLES}"
 echo "Static build       ${STATIC_BUILD}"
+echo "Coverage           ${GEN_COVERAGE}"
 
-CMAKE_CXX_FLAGS="--coverage -g -O0"
+CMAKE_CXX_FLAGS="-g -O0"
 
 if [ "${BUILD_VARIANT}" == "release" ]; then
-    CMAKE_CXX_FLAGS="--coverage -s -g -O3"
+    CMAKE_CXX_FLAGS="-s -g -O3"
+fi
+
+if [ "${GEN_COVERAGE}" == "ON" ]; then
+  CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} --coverage"
 fi
 
 mkdir -p build && cd build
@@ -128,7 +139,6 @@ if [[ "${BUILD_ARCH}" != "${HOST_ARCH}" ]]; then
   echo "Setting up cross compilation toolchain..."
   XCOMPILE_TOOLCHAIN_FILE="-DCMAKE_TOOLCHAIN_FILE=../cmake/${BUILD_ARCH}_to_${HOST_ARCH}.cmake"
 fi
-
 
 # Configure CMake and build the project.
 cmake --no-warn-unused-cli \
---
+++ test_package/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "sdk/IPubSubClient.h"
+#include "sdk/VehicleApp.h"
+#include "sdk/vdb/IVehicleDataBrokerClient.h"
+
+#include <memory>
+
+int main() {
+    const auto vehicleApp = std::make_unique<velocitas::VehicleApp>(
+        velocitas::IVehicleDataBrokerClient::createInstance("vehicledatabroker"),
+        velocitas::IPubSubClient::createInstance("SetDataPointsApp"));
+    vehicleApp->onStart();
+    vehicleApp->onStop();
+    return 0;
+}
